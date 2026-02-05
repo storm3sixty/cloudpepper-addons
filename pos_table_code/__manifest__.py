4d502e6a6f09aadd1d91fd7fdf39b23cd54db58d
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-05 23:41:04 +0000

    Update __manifest__.py

--- pos_table_code/__manifest__.py
+++ pos_table_code/__manifest__.py
@@ -1,21 +1,25 @@
 {
     "name": "POS Table Alphanumeric Codes",
     "version": "19.0.1.0.0",
+    "summary": "Add alphanumeric codes to restaurant tables in POS",
+    "category": "Point of Sale",
+    "author": "Your Name",
     "depends": ["pos_restaurant"],
     "data": [
         "views/restaurant_table_views.xml",
     ],
     "assets": {
         "point_of_sale.assets": [
-            "static/src/js/load_table_code.js",
-            "static/src/js/table_label.js",
+            "pos_table_code/static/src/js/load_table_code.js",
+            "pos_table_code/static/src/js/table_label.js",
         ],
     },
     "installable": True,
-    "depends": ["pos_restaurant"],
     "application": False,
     "auto_install": False,
 }
+
+
 
 
 
