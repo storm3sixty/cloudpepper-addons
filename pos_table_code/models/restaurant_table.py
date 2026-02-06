b4f2e51c6c3f53b6270a5d3ed07b08df9e367fcc
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 18:58:12 +0000

    Update restaurant_table.py

--- pos_table_code/models/restaurant_table.py
+++ pos_table_code/models/restaurant_table.py
@@ -3,23 +3,4 @@ from odoo import models, fields
 class RestaurantTable(models.Model):
     _inherit = 'restaurant.table'
 
-    table_code = fields.Char(string='Table Name')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    table_code = fields.Char(string='Table Name')  # This allows letters, numbers, anything
