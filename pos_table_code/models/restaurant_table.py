3fb27da6812532ad0cd831d3c46021fa51df5d27
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 18:45:02 +0000

    Update restaurant_table.py

--- pos_table_code/models/restaurant_table.py
+++ pos_table_code/models/restaurant_table.py
@@ -1,10 +1,11 @@
-# models/restaurant_table.py
 from odoo import models, fields
 
 class RestaurantTable(models.Model):
-    _inherit = "restaurant.table"
+    _inherit = 'restaurant.table'
 
-    table_code = fields.Char(string="Table Code")
+    table_code = fields.Char(string='Table Name', required=True)
+
+
 
 
 
