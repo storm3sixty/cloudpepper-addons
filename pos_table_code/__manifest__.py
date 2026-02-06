f6cab6ad0c03def4f5eea52a50fe21b191e6216e
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 19:46:04 +0000

    Update __manifest__.py

--- pos_table_code/__manifest__.py
+++ pos_table_code/__manifest__.py
@@ -1,20 +1,30 @@
 {
-    "name": "POS Table Code",
-    "version": "18.0.1.0.0",
-    "category": "Point of Sale",
-    "summary": "Add custom table names for POS",
-    "author": "Your Name",
-    "depends": ["point_of_sale", "restaurant",
-    "data": [
-        "views/x_table_code_views.xml",
+    'name': "POS Table Code",
+    'version': '1.0',
+    'summary': "Allows text table names in POS restaurant tables",
+    'description': """
+        This module allows using text-based table names in POS restaurant tables.
+        It adds a new field 'Table Name / Code' to the restaurant table model.
+        The table name will appear in POS, on kitchen receipts, and payment receipts.
+    """,
+    'author': "Your Name",
+    'website': "https://www.yourcompany.com",
+    'category': 'Point of Sale',
+    'license': 'LGPL-3',
+    'depends': [
+        'point_of_sale',
+        'pos_restaurant',  # Make sure this module is installed
     ],
-    "assets": {
-        "point_of_sale.assets": [
-            "pos_table_code/static/src/js/load_table_code.js",
-            "pos_table_code/static/src/js/table_label.js",
+    'data': [
+        'views/x_table_code_views.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_table_code/static/src/js/load_table_code.js',
+            'pos_table_code/static/src/js/table_label.js',
         ],
     },
-    "installable": True,
-    "application": False,
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
-
