2ab3c2f3b4cf1b417f53126a5db820511e2a27d9
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 15:39:52 +0000

    Update load_table_code.js

--- pos_table_code/static/src/js/load_table_code.js
+++ pos_table_code/static/src/js/load_table_code.js
@@ -1,11 +1,20 @@
 odoo.define('pos_table_code.load_table_code', function(require){
     "use strict";
 
-    const models = require('point_of_sale.models');
+    const PosModel = require('point_of_sale.models');
 
-    // Example: extend POS model to load table_code
-    const PosModelSuper = models.PosModel.prototype.models;
-    // Add your custom logic here if needed
+    // Extend table model to show name instead of number
+    PosModel.PosModel = PosModel.PosModel.extend({
+        initialize: function (session, attributes) {
+            this._super(session, attributes);
+            this.tables.forEach((table) => {
+                table.display_name = table.name || table.number.toString();
+            });
+        },
+    });
+
 });
+
+
 
 
