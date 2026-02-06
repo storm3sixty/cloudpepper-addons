d601cd4f5b61ad41bd3b1158eaf04a4323b0e9ea
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 20:32:30 +0000

    Update __manifest__.py

--- pos_table_code/__manifest__.py
+++ pos_table_code/__manifest__.py
@@ -10,6 +10,11 @@
     'data': [
         'views/x_table_code_views.xml',
     ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_table_code/static/src/js/pos_table_name.js',
+        ],
+    },
     'installable': True,
     'application': False,
     'auto_install': False,
