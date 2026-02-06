436614cab5a84944eb516465293e77d38da1174b
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 19:34:03 +0000

    Update restaurant_table.py

--- pos_table_code/models/restaurant_table.py
+++ pos_table_code/models/restaurant_table.py
@@ -3,4 +3,4 @@ from odoo import models, fields
 class RestaurantTable(models.Model):
     _inherit = 'restaurant.table'
 
-    x_table_code = fields.Char(string='Table Name')  # your custom field
+    x_table_code = fields.Char(string='Table Name')
