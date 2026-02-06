e56d0c7e92f61635df360433fda72ecd1ebb9dd8
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 19:05:54 +0000

    Update load_table_code.js

--- pos_table_code/static/src/js/load_table_code.js
+++ pos_table_code/static/src/js/load_table_code.js
@@ -1,20 +1,18 @@
-odoo.define('pos_table_code.load_table_code', function(require){
+odoo.define('pos_table_code.floor_plan', function(require){
     "use strict";
 
-    const PosModel = require('point_of_sale.models');
+    var models = require('point_of_sale.models');
+    var screens = require('point_of_sale.screens');
 
-    // Extend table model to show name instead of number
-    PosModel.PosModel = PosModel.PosModel.extend({
-        initialize: function (session, attributes) {
-            this._super(session, attributes);
-            this.tables.forEach((table) => {
-                table.display_name = table.name || table.number.toString();
+    var _super = models.PosModel.prototype;
+    models.PosModel = models.PosModel.extend({
+        initialize: function(session, attributes){
+            _super.initialize.call(this, session, attributes);
+            // override tables to use table_code
+            this.tables = this.tables.map(function(table){
+                table.name = table.table_code || table.table_number;
+                return table;
             });
         },
     });
-
 });
-
-
-
-
