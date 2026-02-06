a6ec6c500e3763c39999653740fb86e1341e4924
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 17:43:25 +0000

    Update __manifest__.py

--- pos_table_code/__manifest__.py
+++ pos_table_code/__manifest__.py
@@ -1,14 +1,19 @@
 {
     "name": "POS Table Code",
     "version": "18.0.1.0.0",
+    "summary": "Add custom codes to restaurant tables",
     "category": "Point of Sale",
+    "author": "Your Name",
     "depends": ["pos_restaurant"],
     "data": [
         "views/restaurant_table_views.xml",
     ],
     "installable": True,
+    "application": False,
     "auto_install": False,
 }
+
+
 
 
 
