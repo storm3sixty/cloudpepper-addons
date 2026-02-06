a440b8aafa3168e870f23b22c4f7ccdf890b45b6
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 19:47:57 +0000

    Update load_table_code.js

--- pos_table_code/static/src/js/load_table_code.js
+++ pos_table_code/static/src/js/load_table_code.js
@@ -1,20 +1,7 @@
-odoo.define('pos_table_code.table_labels', function(require){
+odoo.define('pos_table_code.load_table_code', function(require){
     "use strict";
 
-    var screens = require('point_of_sale.screens');
+    const models = require('point_of_sale.models');
 
-    screens.FloorplanWidget.include({
-        render_element: function(){
-            this._super();
-            var self = this;
-            this.pos.tables.forEach(function(table){
-                var $el = self.$('.table[data-id="'+table.id+'"]');
-                if($el.length){
-                    // Use x_table_code if set, otherwise table_number
-                    var label = table.x_table_code || table.table_number;
-                    $el.find('.label').text(label);
-                }
-            });
-        },
-    });
+    models.load_fields('restaurant.table', ['table_code']);
 });
