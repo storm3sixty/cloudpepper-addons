dc1c10b29f81028a5149c05b61108d18dfc63312
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 16:18:24 +0000

    Update restaurant_table.py

--- pos_table_code/models/restaurant_table.py
+++ pos_table_code/models/restaurant_table.py
@@ -3,16 +3,9 @@ from odoo import models, fields
 class RestaurantTable(models.Model):
     _inherit = 'restaurant.table'
 
-    name = fields.Char(string='Table Name', required=True)
-    number = fields.Integer(string='Table Number')
+    table_code = fields.Char(string="Table Code")
 
-    def name_get(self):
-        result = []
-        for table in self:
-            # Use custom name if available, otherwise fallback to number
-            display_name = table.name if table.name else str(table.number)
-            result.append((table.id, display_name))
-        return result
+
 
 
 
