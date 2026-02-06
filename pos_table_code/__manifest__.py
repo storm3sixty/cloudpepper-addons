5d7d7fcb54e9b9f62a3c3cae4fdd47847601c177
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-06 19:35:52 +0000

    Update __manifest__.py

--- pos_table_code/__manifest__.py
+++ pos_table_code/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "POS Table Code",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Point of Sale",
     "summary": "Add custom table names for POS",
     "author": "Your Name",
@@ -11,6 +11,7 @@
     "assets": {
         "point_of_sale.assets": [
             "pos_table_code/static/src/js/load_table_code.js",
+            "pos_table_code/static/src/js/table_label.js",
         ],
     },
     "installable": True,
