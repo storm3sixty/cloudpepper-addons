ef31284586fee929f4d3d9b87ee7289b7a385e31
kajeevan1992 <kajeevan1992@icloud.com>
2026-02-07 15:24:38 +0000

    Fix module upgrade parse error by removing fragile pos.config view inherit

--- pos_table_code/__manifest__.py
+++ pos_table_code/__manifest__.py
@@ -8,9 +8,7 @@
     "website": "https://cloudhopper.example.com",
     "license": "LGPL-3",
     "depends": ["point_of_sale", "pos_restaurant"],
-    "data": [
-        "views/pos_config_views.xml",
-    ],
+    "data": [],
     "assets": {
         "point_of_sale._assets_pos": [
             "pos_table_code/static/src/js/table_name_patch.js",
